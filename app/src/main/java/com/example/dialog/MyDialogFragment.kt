e739d30635222f428c9a81035873e470b176a320
User <ahmadovmuhammadkarim3@>
2023-08-29 13:57:17 +0500

    for 13th group

---
+++ app/src/main/java/com/example/dialog/MyDialogFragment.kt
@@ -0,0 +1,40 @@
+package com.example.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import com.example.dialog.databinding.FragmentMyDialogBinding
+
+
+class MyDialogFragment : DialogFragment() {
+
+    private val binding by lazy { FragmentMyDialogBinding.inflate(layoutInflater) }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+
+
+        binding.cancel.setOnClickListener {
+            Toast.makeText(context, "Cancel", Toast.LENGTH_SHORT).show()
+            dismiss()
+        }
+        binding.submit.setOnClickListener {
+            val selectedId = binding.radioGrup.checkedRadioButtonId
+            val radio = binding.root.findViewById<RadioButton>(selectedId)
+            var ratinResult = radio.text.toString()
+
+            Toast.makeText(context, "Kayfiyatingiz: $ratinResult", Toast.LENGTH_SHORT).show()
+
+            dismiss()
+        }
+
+        return binding.root
+    }
+
+
+}
